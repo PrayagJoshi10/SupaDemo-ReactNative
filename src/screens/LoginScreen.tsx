dd278cad51faf91600b8b2bd6b08c18c6216463f
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-07 16:05:02 +0530

    feat: Added supabase login logic

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,24 +1,68 @@
 import {
+  Alert,
   KeyboardAvoidingView,
   Platform,
   Pressable,
   StyleSheet,
   Text,
   View,
+  Dimensions,
+  ActivityIndicator,
 } from 'react-native';
 import React, {useState} from 'react';
 import colors from '../utils/colors';
 import Header from '../components/Header';
 import CustomInput from '../components/CustomInput';
 import PrimaryButton from '../components/PrimaryButton';
+import {supabase} from '../utils/supabase';
 
 interface Props {
   navigation: any;
 }
-
+const screen_height = Dimensions.get('window').height;
+const screen_width = Dimensions.get('window').width;
 const LoginScreen = ({navigation}: Props) => {
-  const [text, setText] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+
+  const validateEmail = () => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  };
+
+  const signInWithEmail = async () => {
+    setLoading(true);
+    const {error: signinError} = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (signinError) {
+      Alert.alert(signinError.message);
+    }
+    setLoading(false);
+  };
+
+  const onLogin = () => {
+    if (!email) {
+      setError('Email is required');
+      return;
+    } else if (!validateEmail()) {
+      setError('Please enter a valid email');
+      return;
+    } else if (!password) {
+      setError('Password is required');
+      return;
+    } else {
+      signInWithEmail();
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -32,7 +76,7 @@ const LoginScreen = ({navigation}: Props) => {
         <Text style={styles.description}>Login to continue</Text>
       </View>
       <View style={styles.inputContainer}>
-        <CustomInput label="Username" value={text} onChangeText={setText} />
+        <CustomInput label="Username" value={email} onChangeText={setEmail} />
         <CustomInput
           label="Password"
           value={password}
@@ -44,8 +88,9 @@ const LoginScreen = ({navigation}: Props) => {
           <Text style={styles.forgotPasswordButtonLabel}>Forgot password?</Text>
         </Pressable>
       </View>
+      <Text style={styles.error}>{error}</Text>
       <View style={styles.buttonContainer}>
-        <PrimaryButton title="Login" onPress={() => console.log('login')} />
+        <PrimaryButton title="Login" onPress={onLogin} />
         <View style={styles.bottomTextContainer}>
           <Text style={styles.bottomLabel}>Don't have an account?</Text>
           <Text
@@ -55,6 +100,11 @@ const LoginScreen = ({navigation}: Props) => {
           </Text>
         </View>
       </View>
+      {loading && (
+        <View style={styles.loader}>
+          <ActivityIndicator size={'large'} color={colors.camel_500} />
+        </View>
+      )}
     </KeyboardAvoidingView>
   );
 };
@@ -101,8 +151,14 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
   },
+  error: {
+    color: 'red',
+    fontSize: 14,
+    fontWeight: '400',
+    marginTop: 10,
+  },
   buttonContainer: {
-    marginTop: 50,
+    marginTop: 40,
   },
   bottomTextContainer: {
     marginTop: 35,
@@ -124,5 +180,14 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
     paddingVertical: 10,
     paddingRight: 10,
+  },
+  loader: {
+    position: 'absolute',
+    height: screen_height,
+    width: screen_width,
+    backgroundColor: colors.white,
+    opacity: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
