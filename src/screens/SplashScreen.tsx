7361dc96afca00e82c3fd6e4f7baa9bd00b30f94
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-07 16:02:28 +0530

    feat: Designed SplashScreen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,16 +1,37 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import colors from '../utils/colors';
 
-interface Props {}
-
-const SplashScreen = (props: Props) => {
+const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>SupaDemo</Text>
+      <Image
+        source={require('../assets/images/supabase-logo.png')}
+        style={styles.image}
+        resizeMode="contain"
+      />
     </View>
   );
 };
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.blue_500,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 36,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  image: {
+    height: 200,
+    width: 200,
+  },
+});
