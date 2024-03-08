0eba4d36b01495cd65fa3a9c6427b0adbc1bf8f8
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-08 13:12:34 +0530

    feat: Added code for upload and edit image to supabase storage

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,19 @@
-import {Alert, Image, StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import colors from '../utils/colors';
 import PrimaryButton from '../components/PrimaryButton';
 import {supabase} from '../utils/supabase';
 import {useAppProvider} from '../providers/AppProvider';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {decode} from 'base64-arraybuffer';
 
 interface user {
   name: string;
@@ -15,6 +25,9 @@ interface user {
 const HomeScreen = () => {
   const {setSession, session} = useAppProvider();
   const [user, setUser] = useState<user | null>();
+  const [url, setUrl] = useState<string | null>();
+  const [loading, setLoading] = useState<boolean>(false);
+
   const onLogout = async () => {
     const {error} = await supabase.auth.signOut();
 
@@ -36,18 +49,166 @@ const HomeScreen = () => {
         return;
       }
       setUser(data[0]);
+      data[0]?.profile_image && getSignedUrl(data[0]?.profile_image);
     };
 
     getUser();
   }, [session?.user.id]);
 
+  const openImageGallery = () => {
+    launchImageLibrary(
+      {
+        mediaType: 'photo',
+        includeBase64: true,
+        includeExtra: true,
+      },
+      response => {
+        if (response.didCancel) {
+          console.log(response.didCancel, 'cancelled');
+        } else {
+          uploadImage(response);
+        }
+      },
+    );
+  };
+  function getImageExtension(fileName: any) {
+    const lastIndex = fileName.lastIndexOf('.');
+
+    if (
+      lastIndex !== -1 &&
+      lastIndex !== 0 &&
+      lastIndex !== fileName.length - 1
+    ) {
+      const extension = fileName.slice(lastIndex + 1).toLowerCase();
+      return extension;
+    }
+
+    return null;
+  }
+
+  const updateSupabaseImage = async (imageName: any, base64: any) => {
+    const {error} = await supabase.storage
+      .from('images')
+      .update(`${imageName}`, base64, {
+        contentType: 'image/png',
+      });
+
+    if (error) {
+      console.log('[uploadToSupabase] update: ', error);
+      return false;
+    }
+
+    return true;
+  };
+
+  const uploadSupabaseImage = async (imageName: any, base64: any) => {
+    const {error} = await supabase.storage
+      .from('images')
+      .upload(`${imageName}`, base64, {
+        contentType: 'image/png',
+      });
+
+    if (error) {
+      console.log('[uploadToSupabase] upload: ', error);
+      return false;
+    }
+    return true;
+  };
+
+  const uploadImage = async (image: any) => {
+    const res = decode(image.assets[0].base64);
+
+    const imageName =
+      session?.user.id + '.' + getImageExtension(image.assets[0].fileName);
+
+    if (!(res.byteLength > 0)) {
+      console.log('[uploadToSupabase] ArrayBuffer is null');
+      return null;
+    }
+    setLoading(true);
+    const uploadResponse = user?.profile_image
+      ? await updateSupabaseImage(imageName, res)
+      : await uploadSupabaseImage(imageName, res);
+
+    if (!uploadResponse) {
+      Alert.alert('Error uploading image');
+      setLoading(false);
+      return null;
+    }
+
+    const {error: updateError} = await supabase
+      .from('users')
+      .update({profile_image: imageName})
+      .eq('id', session?.user.id);
+    if (updateError) {
+      setLoading(false);
+      Alert.alert(updateError.message);
+      return;
+    }
+
+    const {data, error: getUserError} = await supabase
+      .from('users')
+      .select()
+      .eq('id', session?.user.id);
+    if (getUserError) {
+      Alert.alert(getUserError.message);
+      setLoading(false);
+      return;
+    }
+
+    data && setUser(data[0]);
+    getSignedUrl(imageName);
+  };
+
+  const getSignedUrl = async (image: string) => {
+    setLoading(true);
+    try {
+      const {data} = await supabase.storage
+        .from('images')
+        .createSignedUrl(image, 3600);
+
+      setUrl(data?.signedUrl);
+      return data?.signedUrl;
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.userDetailsContainer}>
-        <Image
-          source={require('../assets/images/user.png')}
-          style={styles.userImage}
-        />
+        <View>
+          <View>
+            {url ? (
+              <Image
+                source={{uri: url}}
+                style={styles.userImage}
+                onLoadEnd={() => setLoading(false)}
+              />
+            ) : (
+              <Image
+                source={require('../assets/images/user.png')}
+                style={styles.userImage}
+              />
+            )}
+            {loading && (
+              <View style={styles.loader}>
+                <ActivityIndicator size={'large'} color={colors.blue_500} />
+              </View>
+            )}
+          </View>
+          <View style={styles.editImageContaier}>
+            <TouchableOpacity
+              style={styles.editImageButton}
+              onPress={openImageGallery}>
+              <Image
+                source={require('../assets/images/edit.png')}
+                style={styles.editIcon}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
         <Text style={styles.name}>{user?.name}</Text>
         <Text style={styles.contact}>Email: {user?.email}</Text>
         <Text style={styles.contact}>Phone: {user?.phone}</Text>
@@ -93,5 +254,27 @@ const styles = StyleSheet.create({
     color: colors.white,
     fontSize: 16,
     fontWeight: '500',
+  },
+  editImageContaier: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  editImageButton: {
+    padding: 10,
+  },
+  editIcon: {
+    height: 20,
+    width: 20,
+    tintColor: colors.white,
+  },
+  loader: {
+    position: 'absolute',
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+    backgroundColor: colors.white,
+    opacity: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
